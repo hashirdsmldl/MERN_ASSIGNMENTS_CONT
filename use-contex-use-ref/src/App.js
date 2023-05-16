cb05ed31c7e41721437e37229b50c15a41f0c41c
hashii009 <hashirawan76@gmail.com>
2023-05-16 23:17:25 +0500

    commited;

---
+++ use-contex-use-ref/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <MathOperation title="sum" />
+      <MathOperation title="divide" />
+      <MathOperation title="minus" />
+    
+    </div>
+  );
+}
+
+export default App;
