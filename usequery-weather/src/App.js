5a6883e1fa2d4cb1788ed6733c1b0fcc045048e3
Hashir Inayat <inayahas@corp.contoursoftware.net>
2023-05-16 12:07:11 +0500

    'pushed'

--- usequery-weather/src/App.js
+++ usequery-weather/src/App.js
@@ -1,5 +1,6 @@
+import axios from "axios";
 import React, { useState } from "react";
-import { useQuery } from "react-query";
+import { useQuery } from "@tanstack/react-query"
 
 
 
@@ -7,48 +8,77 @@ import { useQuery } from "react-query";
 const App = () => {
   const [cityName, setCityName] = useState("");
 
-  
-  const fetchWeatherData = async (cityName) => {
-  //  const cityName = "London";
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=250d4ebd906279665f1ad04f46b48a73&units=metric`;
-  
-    const res = await fetch(url);
-    return res.json();
-  };
-  const { data,  status } = useQuery(["user", { cityName }], (cityName) =>
-  fetchWeatherData(cityName)
-);
-  
-  const handleValue=(e)=>
-  {
-  const value=e.target.value;
 
-   
-     
-     setCityName(value) 
 
-    
+  const [fetch, setFetch] = useState(false)
+
+
+  const {
+    data: posts,
+    isFetching,
+    isInitialLoading,
+    isError,
+    error,
+    refetch,
+  } = useQuery(
+    ["posts"],
+    async () => {
+      if (cityName) {
+        const { data } = await axios.get(
+          "https://api.openweathermap.org/data/2.5/weather?q=" +
+            cityName +
+            "&appid=250d4ebd906279665f1ad04f46b48a73&units=metric"
+        );
+        return data;
+      }
+      return null;
+    },
+    { enabled: fetch }
+  );
+  const handleFetchPosts = () => {
+
+    setFetch(true)
+
+    refetch()
 
   }
 
-  
- 
+
+  const handleValue = (e) => {
+    const value = e.target.value;
+    setCityName(value);
+
+
+
+  }
+
+
+
+
 
   return (
     <div className="App">
-<input type="text" onChange={handleValue}></input>
-{cityName && (<>
-      {status === "error" && <p>Error fetching data</p>}
-      {status === "loading" && <p>Fetching data...</p>}
-      {status === "success" && (
+      <input value={cityName} type="text" onChange={handleValue}></input>
+      <button onClick={handleFetchPosts}>Click</button>
+      <h1 className="mb-6 text-blue-400 text-center">
+        {isFetching && "Fetching weather..."}
+      </h1>
+      {posts ? (
         <div>
-          <p>Temperature: {data.main.temp}</p>
-          <p>City: {data.name}</p>
+          <h1 className="w-1/2 m-auto">{posts.main.temp}</h1>
+          <h1 className="w-1/2 m-auto">{posts.name}</h1>
         </div>
+      ) : isInitialLoading ? (
+        <h1 className="text-blue-400 text-center">Loading...</h1>
+      ) : isError ? (
+        <h1>{error.message}</h1>
+      ) : (
+        ""
       )}
-      </>)}
     </div>
   );
+
+
 };
 
 export default App;
